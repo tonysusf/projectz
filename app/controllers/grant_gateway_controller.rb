d2ea2100520d6a962802087377d55664bacacc9d
Ian Smith <iansmith9876@gmail.com>
2015-02-08 09:43:53 -0800

    Clean up code, remove unused code

--- app/controllers/grant_gateway_controller.rb
+++ app/controllers/grant_gateway_controller.rb
@@ -1,29 +1,27 @@
 class GrantGatewayController < ApplicationController
-  
-  
-  def data
 
+  def data
     response = [Question.all, QuestionOption.all]
-    
+
     respond_to do | format |
       format.json { render :text => response.to_json }
     end
   end
-  
-  
+
+
   def search
     hash = {}
-    
+
     params.each do |key, val|
       if(key.index('question-') == 0)
         hash[key.gsub('question-', '')] = val unless val.blank?
       end
     end
-    
+
     response = GrantProcessor.filter(hash)
     respond_to do | format |
       format.json { render :text => response.to_json }
     end
   end
-  
+
 end
