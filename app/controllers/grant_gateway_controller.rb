c2f153fa4badd49dc10e2d1f78f0014e0184313d
Tony Su <makefire@gmail.com>
2015-02-07 20:56:18 -0800

    grant processor

--- app/controllers/grant_gateway_controller.rb
+++ app/controllers/grant_gateway_controller.rb
@@ -12,53 +12,18 @@ class GrantGatewayController < ApplicationController
   
   
   def search
-    response = Grant.all
+    hash = {}
     
+    params.each do |key, val|
+      if(key.index('question-') == 0)
+        hash[key.gsub('question-', '')] = val unless val.blank?
+      end
+    end
+    
+    response = GrantProcessor.filter(hash)
     respond_to do | format |
       format.json { render :text => response.to_json }
     end
   end
   
-  private
-  def get_fake_questions
-    data = []
-    q = Question.new
-    q.id = 1
-    q.text = 'Are you a US Citizen'
-    
-    data << q
-    data
-  end
-  
-  def get_fake_options
-    data = []
-    opt1 = QuestionOption.new
-    opt1.id = 1
-    opt1.text = 'Y'
-    opt1.question_id = 1
-    
-    opt2 = QuestionOption.new
-    opt2.id = 2
-    opt2.text = 'N'
-    opt2.question_id = 1
-    
-    data << opt1
-    data << opt2
-    data
-  end
-  
-  def get_fake_grants
-    data = []
-    obj = Grant.new
-    obj.id = 1
-    obj.name = 'Dream Maker Grant'
-    data << obj
-    
-    obj = Grant.new
-    obj.id = 2
-    obj.name = 'Good Neighbor Next Door'
-    data << obj
-    
-    data
-  end
 end
--- app/models/grant_processor.rb
+++ app/models/grant_processor.rb
@@ -1,3 +1,40 @@
 class GrantProcessor
   
+  def self.filter(question_answer_hash)
+    
+    grants = Grant.all
+    results = []
+    grants.each do |grant|
+      pass = true
+      question_answer_hash.each do |question_id, answer|
+        question = Question.find(question_id)
+        match = match_question(grant, question, answer)
+        expected = GrantCondition.where(:grant_id => grant.id, :question_id => question.id)
+        expected_id = 0
+        expected_id = expected.first.question_option_id if expected && expected.first
+        Rails.logger.info "======> checking grant/question/expected/ answer/match: " +
+          "#{grant.id}-#{grant.name}/ #{question.id}-#{question.text}/ " + 
+          "#{expected_id}/ #{answer}/ #{match}"
+        unless match
+          pass = false
+        end
+      end
+      results << grant if pass
+    end
+    results
+  end
+  
+  private
+  def self.match_question(grant, question, answer)
+    result = true
+    if (question.input_type == Question::QUESTION_TYPE_DROPDOWN) 
+      expected = GrantCondition.where(:grant_id => grant.id, :question_id => question.id).try(:first)
+      if (expected)
+        result = false unless answer == expected.question_option_id
+      end
+    elsif (question.input_type == Question::QUESTION_TYPE_TEXT_BOX) 
+      
+    end
+    result
+  end
 end
