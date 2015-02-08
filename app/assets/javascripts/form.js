bc224d51dee8cb636c1f7d325c6616e989a9fb90
Ian Smith <iansmith9876@gmail.com>
2015-02-07 20:22:44 -0800

    Update search result styles and add links

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $('.search').on('click', function() {
     $.post( '/grant_gateway/search', $('#search-form').serialize(), function(data) {
       for (var i = 0; i < data.length; i++) {
-        $('.results').append("<div class='media'><div class='media-body'><h5 class='media-heading'>" + data[i].name + "</h5></div></div>")
+        $('.results').append("<a href='" + data[i].link + "' class='media-links'><div class='media'><div class='media-body'><h5 class='media-heading'>" + data[i].name + "</h5><i class='fa fa-arrow-circle-o-right'></i><br/><p>" + data[i].description + "</p></div></div></a>")
       };
     });
   });
