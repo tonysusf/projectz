75588e8a0dccc98a4eaa40d6945add214ac89411
Ian Smith <iansmith9876@gmail.com>
2015-02-08 11:42:10 -0800

    Only add links to grants that have links

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -15,7 +15,11 @@ $(document).ready(function() {
       var total_value = 0;
       $('.results').empty()
       for (var i = 0; i < data.length; i++) {
-        $('.results').append("<a href='" + data[i].link + "' class='media-links'><div class='media'><div class='media-body'><h5 class='media-heading'>" + data[i].name + "</h5><i class='fa fa-arrow-circle-o-right'></i><br/><p>" + data[i].description + "</p></div></div></a>")
+        if (data[i].link) {
+          $('.results').append("<a href='" + data[i].link + "' class='media-links'><div class='media'><div class='media-body'><h5 class='media-heading'>" + data[i].name + "</h5><i class='fa fa-arrow-circle-o-right'></i><br/><p>" + data[i].description + "</p></div></div></a>")
+        } else {
+          $('.results').append("<div class='media'><div class='media-body'><h5 class='media-heading'>" + data[i].name + "</h5><i class='fa fa-arrow-circle-o-right'></i><br/><p>" + data[i].description + "</p></div></div>")
+        };
         if (data[i].amount > 0) {
           total_value += parseInt(data[i].amount)
         };
